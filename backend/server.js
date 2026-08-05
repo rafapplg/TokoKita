3fe46aec87180d4cebc2989d14ff55de6907e903
rafapplg <mrafaaldnsyh11@gmail.com>
2026-08-05 04:21:48 +0000

    backend langkah 4

--- backend/server.js
+++ backend/server.js
@@ -17,3 +17,19 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
+
+app.get("/api/ping", (req,res) => {
+  res.json({
+    status: "success",
+    message: "pong",
+    waktuServer: new Date().toISOString(),
+  });
+});
+
+let.produk = [
+  { id: 1, nama: "Iphone 17 Pro Max 16/1TB", harga: 1 },
+  { id: 2, nama: "Rolex Dajetsu 41", harga: 207999999 },
+  { id: 3, nama: "Red Magic 10 Pro", harga: 22999000},
+  { id: 4, nama: "Samsung Z Fold 7", harga: 32599000},
+  { id: 5, nama: ""}
+]
